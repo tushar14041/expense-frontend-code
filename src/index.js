6cac5a47b5d46d70f11b4ba7d33817c50cacaf70
tushar khatri <tusharkhtr110@gmail.com>
2020-08-28 16:35:17 +0530

    from the git bash

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Home from './Home';
+import Category from './Category'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import * as serviceWorker from './serviceWorker';
+import AppNav from './AppNav';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
@@ -10,3 +16,16 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+//       AppNav
+//          |
+//          V  
+//      Home.js   Category.js    Expenses.js
+//          |
+//          V
+//       App.js
+//          |
+//          V
+//        index.js
+// 
